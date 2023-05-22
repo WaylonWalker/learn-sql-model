61d2945dc7d35114e51f8fa3d7867d694f6cfc64
Waylon S. Walker <waylon@waylonwalker.com>
2023-05-22 08:06:40 -0500

    change the alembic_revision table

--- migrations/env.py
+++ migrations/env.py
@@ -54,6 +54,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        version_table=f'{config.get_main_option("project")}_alembic_version',
     )
 
     with context.begin_transaction():
@@ -74,7 +75,12 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            render_as_batch=True,
+            version_table=f'{config.get_main_option("project")}_alembic_version',
+        )
 
         with context.begin_transaction():
             context.run_migrations()
